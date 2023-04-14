382d4f22462c88872116041179c029b88b706ce3
AditiGi <aditisingh20144@gmail.com>
2023-04-14 07:35:33 +0530

    form submission

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const App = () => {
+/* const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const toggleTheme = () => {
+   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
 
@@ -18,9 +18,54 @@ const App = () => {
     >
       <h1>Hello World</h1>
       <button onClick={toggleTheme}>
-        {isDarkMode ? "Switch to Light Theme" : "Switch to Dark Theme"}
+        {isDarkMode ? "Light Theme" : "Dark Theme"}
       </button>
     </div>
+  );
+}; */
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
