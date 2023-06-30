6dd18a46d0b5eed82c58aa59b1a2cc81dc153dfa
AditiGi <aditisingh20144@gmail.com>
2023-06-30 20:28:28 +0530

    sjdioa

--- src/components/useEffect.js
+++ src/components/useEffect.js
@@ -1,14 +1,112 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+// const Experiment = () => {
+//   const countries = [
+//     "Japan",
+//     "Peru",
+//     "Philippines",
+//     "Russia",
+//     " China",
+//     "Peru",
+//     "Portugal",
+//     " Russia",
+//     "China",
+//     "Argentina",
+//     "Philippines",
+//     "Indonesia",
+//     "Macedonia",
+//     "Bahamas",
+//     "Venezuela",
+//     "France",
+//     "Poland",
+//     "Japan",
+//     "Indonesia",
+//     "Greece",
+//     " Russia",
+//     "Tanzania",
+//     "US",
+//     "Nigeria",
+//     "Norway",
+//     "Libya",
+//     "Greece",
+//     "Bangladesh",
+//   ];
+//   const cities = [
+//     "Baizhong",
+//     "Jishi",
+//     "Boaco",
+//     "Milano",
+//     "Qikeshu",
+//     "Fangxian",
+//     "Shaxi",
+//     "Xingang",
+//     "Jugantang",
+//     "Paris",
+//     "Courtaboeuf",
+//     "Caujul",
+//     "Londiani",
+//     "Yueshan",
+//     "Kebonbencoy",
+//     "Changtu",
+//     "Boneng",
+//     "Sydney",
+//     "Xiejiatan",
+//     "Hevlín",
+//     "Illapel",
+//     "Sagopshi",
+//     "Wengang",
+//     "Zhouji",
+//     "Castres",
+//     "Rychtal",
+//     "Orléans",
+//     "Mizhirah",
+//     "Liuzuo,",
+//   ];
+
+//   const [city, setCity] = useState(false);
+//   const [country, setCountry] = useState(false);
+
+//   const cityHandle = () => {
+//     setCity(!city);
+//   };
+//   const countryHandle = () => {
+//     setCountry(!country);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={cityHandle}>City</button>
+//       {city && (
+//         <ul>
+//           {cities.map((city, index) => (
+//             <li key={index}>{city}</li>
+//           ))}
+//         </ul>
+//       )}
+//       <button onClick={countryHandle}>Country</button>
+//       {country && (
+//         <ul>
+//           {countries.map((country, index) => (
+//             <li key={index}>{country}</li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+// export default Experiment;
+
+import React, { useState } from "react";
+
 const Experiment = () => {
   const countries = [
     "Japan",
     "Peru",
     "Philippines",
     "Russia",
-    " China",
+    "China",
     "Peru",
     "Portugal",
-    " Russia",
+    "Russia",
     "China",
     "Argentina",
     "Philippines",
@@ -21,7 +119,7 @@ const Experiment = () => {
     "Japan",
     "Indonesia",
     "Greece",
-    " Russia",
+    "Russia",
     "Tanzania",
     "US",
     "Nigeria",
@@ -59,17 +157,28 @@ const Experiment = () => {
     "Rychtal",
     "Orléans",
     "Mizhirah",
-    "Liuzuo,",
+    "Liuzuo",
   ];
 
   const [city, setCity] = useState(false);
   const [country, setCountry] = useState(false);
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   const cityHandle = () => {
     setCity(!city);
   };
+
   const countryHandle = () => {
     setCountry(!country);
+  };
+
+  const handleCitySelection = (selectedCity) => {
+    setSelectedCity(selectedCity);
+  };
+
+  const handleCountrySelection = (selectedCountry) => {
+    setSelectedCountry(selectedCountry);
   };
 
   return (
@@ -78,21 +187,35 @@ const Experiment = () => {
       {city && (
         <ul>
           {cities.map((city, index) => (
-            <li key={index}>{city}</li>
+            <li
+              key={index}
+              onClick={() => handleCitySelection(city)}
+              style={{ cursor: "pointer" }}
+            >
+              {city}
+            </li>
           ))}
         </ul>
       )}
+      {selectedCity && <p>Selected city: {selectedCity}</p>}
+
       <button onClick={countryHandle}>Country</button>
       {country && (
         <ul>
           {countries.map((country, index) => (
-            <li key={index}>{country}</li>
+            <li
+              key={index}
+              onClick={() => handleCountrySelection(country)}
+              style={{ cursor: "pointer" }}
+            >
+              {country}
+            </li>
           ))}
         </ul>
       )}
-
-      <div></div>
+      {selectedCountry && <p>Selected country: {selectedCountry}</p>}
     </div>
   );
 };
+
 export default Experiment;
